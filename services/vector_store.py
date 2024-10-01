d28057088fd303af229ea12f95e057ab41343f36
khachuy7 <huyk93345@gmail.com>
2024-10-01 21:08:53 +0700

    pdf doc

--- services/vector_store.py
+++ services/vector_store.py
@@ -2,6 +2,8 @@ from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain.vectorstores import FAISS
 from utils.session import get_session_id
 import streamlit as st
+import openai
+from langchain.vectorstores import FAISS
 
 # Tạo và lưu vector store
 def get_vector_store(chunks):
@@ -9,4 +11,31 @@ def get_vector_store(chunks):
     embeddings = SentenceTransformerEmbeddings(model_name="keepitreal/vietnamese-sbert", model_kwargs={"trust_remote_code": True})
     vector_store = FAISS.from_texts(chunks, embedding=embeddings)
     vector_store.save_local(f"faiss_index_{session_id}")
+    st.session_state[session_id]['vector_store'] = vector_store
+
+# Hàm lấy embeddings từ OpenAI
+def get_embeddings(text):
+    # Đặt API key của bạn
+    openai.api_key = 'YOUR_API_KEY'
+    response = openai.Embedding.create(
+        input=text,
+        model="text-embedding-ada-002"  # Hoặc một model khác mà bạn muốn
+    )
+    return response['data'][0]['embedding']
+
+
+# Tạo và lưu vector store
+def get_vector_store_openai(chunks):
+    session_id = get_session_id()
+    
+    # Tạo embeddings cho từng chunk
+    embeddings = [get_embeddings(chunk) for chunk in chunks]
+    
+    # Tạo vector store từ embeddings
+    vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+    
+    # Lưu vector store vào file
+    vector_store.save_local(f"faiss_index_{session_id}")
+    
+    # Lưu vào session state
     st.session_state[session_id]['vector_store'] = vector_store
--- utils/embeddings.py
+++ utils/embeddings.py
@@ -1,5 +1,16 @@
 from langchain_community.embeddings import SentenceTransformerEmbeddings
-
+import openai
 # Trả về Embeddings
 def get_embeddings():
     return SentenceTransformerEmbeddings(model_name="keepitreal/vietnamese-sbert", model_kwargs={"trust_remote_code": True})
+
+
+# Trả về Embeddings
+def get_embeddings_OpenAI(text):
+    # Đặt API key của bạn
+    openai.api_key = 'YOUR_API_KEY'
+    response = openai.Embedding.create(
+        input=text,
+        model="text-embedding-ada-002"  # Hoặc một model khác mà bạn muốn
+    )
+    return response['data'][0]['embedding']
